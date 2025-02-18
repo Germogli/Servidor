ba936487e87e097c008328828ccbb607996babec
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-02-17 20:37:33 -0500

    first commit

---
+++ src/test/java/com/germogli/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germogli.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
