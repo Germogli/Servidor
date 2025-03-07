3b33d4fbac7fdc79efe8a86644185af11f053c49
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-03-07 10:24:38 -0500

    se implementan las variables de entorno

--- src/main/java/com/germogli/backend/BackendApplication.java
+++ src/main/java/com/germogli/backend/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @EnableJpaRepositories(basePackages = {
 		"com.germogli.backend.community.post.infrastructure.crud",
 		"com.germogli.backend.community.post.domain.repository",
@@ -20,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"com.germogli.backend.common",
 		"com.germogli.backend.authentication" // Se tiene que agregar para escanear el módulo de autenticacion
 })
+@SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
