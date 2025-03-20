bad637fb12eac491f3d41055f643f9d3ab5b5495
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-03-20 11:46:13 -0500

    feat(education): se crean las entidades tanto para modulo como para etiqueta con su respectiva relacion, incluyendo la tabla intermedia 'module_tags'

---
+++ src/main/java/com/germogli/backend/education/module/infrastructure/entity/ModuleEntity.java
@@ -0,0 +1,2 @@
+package com.germogli.backend.education.module.infrastructure.entity;public class ModuleEntity {
+}
---
+++ src/main/java/com/germogli/backend/education/tag/infrastructure/entity/TagEntity.java
@@ -0,0 +1,2 @@
+package com.germogli.backend.education.tag.infrastructure.entity;public class TagEntity {
+}
