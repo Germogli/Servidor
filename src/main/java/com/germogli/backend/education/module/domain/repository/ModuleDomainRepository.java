6e29a35051f5e203cd63e1046a17245ef537d21a
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-03-20 12:10:38 -0500

    feat(education): Se crean interfaces DomainRepository para modulos y etiquetas vacias

---
+++ src/main/java/com/germogli/backend/education/module/domain/repository/ModuleDomainRepository.java
@@ -0,0 +1,6 @@
+package com.germogli.backend.education.module.domain.repository;
+
+import java.util.List;
+
+public interface ModuleDomainRepository {
+}
---
+++ src/main/java/com/germogli/backend/education/tag/domain/repository/TagDomainRepository.java
@@ -0,0 +1,4 @@
+package com.germogli.backend.education.tag.domain.repository;
+
+public interface TagDomainRepository {
+}
